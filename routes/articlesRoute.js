926b650e7bd4028938ee338ca9cabe3890495569
natasafi <natasafrag@gmail.com>
2019-04-03 16:51:08 +0100

    added test for array length of topics array in default beheaviours

--- routes/articlesRoute.js
+++ routes/articlesRoute.js
@@ -1,6 +1,10 @@
 const articlesRouter = require("express").Router();
 const { getArticles } = require("../controllers/articlesController");
+const { methodNotAllowed } = require("../errors/index");
 
-articlesRouter.route("/").get(getArticles);
+articlesRouter
+  .route("/")
+  .get(getArticles)
+  .all(methodNotAllowed);
 
 module.exports = articlesRouter;
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -26,9 +26,17 @@ describe("/", () => {
               });
             });
         });
+        it("GET STATUS 200 by sending back an array of 2 topics", () => {
+          return request
+            .get("/api/topics")
+            .expect(200)
+            .then(({ body }) => {
+              expect(body.topics).to.have.lengthOf(2);
+            });
+        });
       });
       describe("ERROR HANDLING FOR TOPICS", () => {
-        it("STATUS 505 for invalid method", () => {
+        it("STATUS 405 for invalid method for topics", () => {
           const invalidMethods = ["post", "patch", "delete", "put"];
           const methodPromises = invalidMethods.map(method =>
             request[method]("/api/topics").expect(405)
@@ -57,6 +65,15 @@ describe("/", () => {
                 );
               });
             });
+        });
+      });
+      describe("ERROR HANDLING FOR ARTICLES ", () => {
+        it('"STATUS 405 for invalid method for articles"', () => {
+          const invalidMethods = ["post", "patch", "delete", "put"];
+          const methodPromises = invalidMethods.map(method =>
+            request[method]("/api/topics").expect(405)
+          );
+          return Promise.all(methodPromises);
         });
       });
     });
